fa0fd3b4f03fe03c8685cdaab1ca3ca84adf4eae
SavannaWilliams <54858237+SavannaWilliams@users.noreply.github.com>
2020-08-22 17:36:49 -0400

    Add Traffic line chart and style it.

--- js/charts.js
+++ js/charts.js
@@ -1,9 +1,60 @@
 const purple = '#7477bf';
 const teal = '#73b1bf';
 const green = '#81c98f';
+const white = '#fff';
 
 Chart.defaults.global.responsive = true;
 
+// Traffic Chart
+
+var tc = document.getElementById('trafficChart').getContext('2d');
+var myChart = new Chart(tc, {
+    type: 'line',
+    data: {
+        labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
+        datasets: [{
+            label: '# of Weekly Visistors',
+            data: [500, 1000, 750, 1250, 1750, 1250, 1500, 1000, 1500, 2000, 1500, 2000],
+            backgroundColor: 'rgba(116, 119, 191, 0.5)',
+            borderColor: purple,
+            borderWidth: 2,
+            pointBackgroundColor: white,
+            pointRadius: 6
+        }]
+    },
+    options: {
+        pointRadius: 8,
+        pointBackgroundColor: '#fff',
+        pointBorderColor: purple,
+        legend: {
+            display: false
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    max: 2500,
+                    stepSize: 500,
+                    labelOffset: 80,
+                    callback: function(value, index) {
+                        if (value !== 0) {
+                            return value;
+                        }
+                    }
+                }
+            }],
+            xAxes: [{
+                barPercentage: 0.6,
+                }]
+        },
+        // Creates straight lines between points
+        elements: {
+            line: {
+                tension: 0
+            }
+        }
+    }
+});
 
 // Daily Traffic Bar Chart
 var dtc = document.getElementById('dailyTrafficChart').getContext('2d');
@@ -27,6 +78,7 @@ var myChart = new Chart(dtc, {
         }]
     },
     options: {
+        maintainAspectRatio: false,
         legend: {
             display: false
         },
@@ -59,7 +111,7 @@ var myChart = new Chart(muc, {
     data: {
         labels: ['Phones', 'Tablets', 'Desktop'],
         datasets: [{
-            label: '# of Daily Visistors',
+            label: '% of Users',
             data: [15, 15,70],
             backgroundColor: [
                 green,
