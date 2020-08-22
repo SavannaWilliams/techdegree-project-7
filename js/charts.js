cb7d08e62e72365bd1feff0ffbd2f85e9e1e657c
SavannaWilliams <54858237+SavannaWilliams@users.noreply.github.com>
2020-08-22 13:21:16 -0400

    Remove zero start on bar graph, adjust labels, and remove legend.

--- js/charts.js
+++ js/charts.js
@@ -2,14 +2,15 @@ let purple = '#7477bf';
 
 Chart.defaults.global.responsive = true;
 
-var ctx = document.getElementById('dailyTrafficChart').getContext('2d');
-var myChart = new Chart(ctx, {
+var dtc = document.getElementById('dailyTrafficChart').getContext('2d');
+var myChart = new Chart(dtc, {
+    
     type: 'bar',
     data: {
         labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
         datasets: [{
             label: '# of Daily Visistors',
-            data: [75, 100, 175, 125, 225, 200, 100],
+            data: [50, 75, 145, 100, 200, 175, 75],
             backgroundColor: [
                 purple,
                 purple,
@@ -23,18 +24,32 @@ var myChart = new Chart(ctx, {
         }]
     },
     options: {
-        curvature: 1,
+        legend: {
+            display: false
+        },
         scales: {
             yAxes: [{
                 ticks: {
                     beginAtZero: true,
                     max: 250,
-                    stepSize: 50
+                    stepSize: 50,
+                    labelOffset: 35,
+                    callback: function(value, index) {
+                        if (value !== 0) {
+                            return value;
+                        }
+                    }
                 }
             }],
             xAxes: [{
-                barPercentage: 0.6
+                barPercentage: 0.6,
                 }]
         }
     }
 });
+
+// window.addEventListener('resize', function (e) {
+//     let size = window.screen.availWidth;
+//     let newSize = dtc.canvas.parentNode.style.width = (size/2);
+//     console.log(newSize);
+// });
