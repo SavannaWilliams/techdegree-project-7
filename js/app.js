fa9826641fdcda224d1e3ccc11b9d90f4f825ee8
SavannaWilliams <54858237+SavannaWilliams@users.noreply.github.com>
2020-09-01 17:31:24 -0400

    Add data and code to swap traffic charts with button clicks.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,120 @@
+// Chart Javascript
+
+const purple = '#7477bf';
+const teal = '#73b1bf';
+const green = '#81c98f';
+const white = '#fff';
+
+// Chart.defaults.global.responsive = true;
+
+// Traffic Chart- Weekly
+let trafficCanvas = document.getElementById('trafficChart');
+
+let trafficData = {
+        labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+        datasets: [{
+            data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+            backgroundColor: 'rgba(116, 119, 191, .5)',
+            borderWidth: 1,
+            lineTension: 0
+        }]
+};
+
+let trafficOptions = {
+        aspectRatio: 2.5,
+        animation: {
+            duration: 0
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    }
+            }],
+        },
+        legend: {
+            display: false
+        },
+};
+
+let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions
+});
+
+// Daily Traffic Bar Chart
+let dailyCanvas = document.getElementById('dailyTrafficChart');
+
+const dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+        label: '# of Hits',
+        data: [75, 115, 175, 125, 225, 200, 100],
+        backgroundColor: purple,
+        borderWidth: 1
+    }]
+    };
+
+    const dailyOptions = {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }],
+        },
+        legend: {
+            display: false
+        }
+    }
+    
+
+let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
+});
+
+
+
+
+// Mobile Users Doughnut Chart
+let mobileCanvas = document.getElementById('mobileUsersChart');
+
+const mobileData = {
+    labels: ["Phones", "Tablet",  "Desktop"],
+    datasets: [{
+        label: '# of Users',
+        data: [500, 550, 2000],
+        borderWidth: 0,
+        backgroundColor: [
+            teal,
+            green,
+            purple,
+        ]
+    }]
+}   
+
+    const mobileOptions = {
+        legend: {
+            position: 'right',
+            labels: {
+                boxWidth: 20,
+                fontStyle: 'bold'
+            }
+        }      
+    }
+    
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
+});
+
+// Main app JavaScript
+
 let alertBanner = document.getElementById('alert');
 let alertX = document.getElementById('alert-x');
 let notifications = document.getElementById('notif-bell');
@@ -213,13 +330,87 @@ let chartSelectors = document.getElementsByClassName('chartOption');
 
 // Check for clicks on different traffic date range options.
 trafficSelectors.addEventListener("click", function (e) {
-  let selectedChart = e.target.parentNode;
+  let selectedChartDiv = e.target.parentNode;
+  let currentChart = e.target;
+  let trafficHeader = document.getElementById('trafficHeader');
 
   // Remove existing active chart styling before applying new styling to newly selected chart.
   for(let i = 0; i < chartSelectors.length; i++) {
-    if (chartSelectors[i].classList.contains('activeChart')) {
-      chartSelectors[i].classList.remove('activeChart');
+    let selectedChart = chartSelectors[i];
+    console.log(selectedChart.id);
+    if (selectedChart !== trafficHeader && selectedChart.classList.contains('activeChart')) {
+      selectedChart.classList.remove('activeChart');
+      selectedChartDiv.classList.add('activeChart');
     }
+  };
+
+
+  if(currentChart.textContent === 'Daily') {
+    trafficData = {
+      labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"],
+      datasets: [{
+          data: [175, 280, 295, 105, 245, 101, 90, 200, 255, 242, 280, 295, 175, 280, 295, 105, 165, 200, 245, 101, 90, 240, 200, 240, 255, 242, 280, 295, 202, 185, 222],
+          backgroundColor: 'rgba(116, 119, 191, .5)',
+          borderWidth: 1,
+          lineTension: 0
+      }]
+    };
+    trafficChart = new Chart(trafficCanvas, {
+      type: 'line',
+      data: trafficData,
+      options: trafficOptions
+    });
   }
-  selectedChart.classList.add('activeChart');
+
+  if(currentChart.textContent === 'Hourly') {
+    trafficData = {
+      labels: ["12 AM", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12 PM", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"],
+      datasets: [{
+          data: [25, 10, 15, 20, 21, 13, 9, 14, 15, 28, 21, 20, 15, 6, 9, 15, 16, 20, 32, 11, 9, 24, 31, 20],
+          backgroundColor: 'rgba(116, 119, 191, .5)',
+          borderWidth: 1,
+          lineTension: 0
+      }]
+    };
+    trafficChart = new Chart(trafficCanvas, {
+      type: 'line',
+      data: trafficData,
+      options: trafficOptions
+    });
+  }
+    
+  if(currentChart.textContent === 'Weekly') {
+    let trafficData = {
+      labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+      datasets: [{
+          data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+          backgroundColor: 'rgba(116, 119, 191, .5)',
+          borderWidth: 1,
+          lineTension: 0
+      }]
+    };
+      trafficChart = new Chart(trafficCanvas, {
+        type: 'line',
+        data: trafficData,
+        options: trafficOptions
+      });
+  }
+
+  if(currentChart.textContent === 'Monthly') {
+    let trafficData = {
+      labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"],
+      datasets: [{
+          data: [2220, 3500, 3900, 4200, 2650, 2180, 3650, 2850, 2050, 4800, 3900, 3750],
+          backgroundColor: 'rgba(116, 119, 191, .5)',
+          borderWidth: 1,
+          lineTension: 0
+      }]
+    };
+      trafficChart = new Chart(trafficCanvas, {
+        type: 'line',
+        data: trafficData,
+        options: trafficOptions
+      });
+  }
+
 });
